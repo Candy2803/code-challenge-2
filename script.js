d999ec11177d4118203087e013fa65106fc59201
+ <+>
2024-07-06 14:46:29 +0300

    commit

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
+
 function addToList() {
     const newItemInput = document.getElementById('new');
   
     const newItem = newItemInput.value.trim();
   
     if (!newItem) {
-      alert('You have not entered any item')
+      alert('You have not entered any item');
       return;
     }
   
     const liItem = document.createElement('li');
-  
     liItem.textContent = newItem;
   
-    liItem.addEventListener('click', function() {
-      this.classList.toggle('purchased'); 
-    });
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-btn'); 
+    deleteButton.textContent = '🚮';
   
+    function deleteItem(event) {
+      const clickedButton = event.target;
+      const listItem = clickedButton.parentElement; 
+      listItem.parentNode.removeChild(listItem); 
+    }
+  
+    deleteButton.addEventListener('click', deleteItem);
+  
+    liItem.appendChild(deleteButton);
     const list = document.getElementById('list');
     list.appendChild(liItem);
   
